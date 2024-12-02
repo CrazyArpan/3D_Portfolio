3524077d78d3fbd7b211c436e9335f397d4325be
CrazyArpan <166271755+CrazyArpan@users.noreply.github.com>
2024-12-02 22:06:28 +0530

    Update index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -17,157 +17,155 @@ import {
     threejs,
     passmanager,
     tquiz
-  } from "../assets";
-  
-  export const navLinks = [
+} from "../assets";
+
+export const navLinks = [
     {
-      id: "about",
-      title: "About",
+        id: "about",
+        title: "About",
     },
     {
-      id: "contact",
-      title: "Contact",
+        id: "contact",
+        title: "Contact",
     },
-  ];
-  
-  const services = [
+];
+
+const services = [
     {
-      title: "Web Developer",
-      icon: web,
-    },
-    {
-      title: "React Native Developer",
-      icon: mobile,
+        title: "Web Developer",
+        icon: web,
     },
     {
-      title: "Backend Developer",
-      icon: backend,
+        title: "React Native Developer",
+        icon: mobile,
     },
     {
-      title: "Video Editor",
-      icon: creator,
-    },
-  ];
-  
-  const technologies = [
-    {
-      name: "HTML 5",
-      icon: html,
+        title: "Backend Developer",
+        icon: backend,
     },
     {
-      name: "CSS 3",
-      icon: css,
+        title: "Video Editor",
+        icon: creator,
+    },
+];
+
+const technologies = [
+    {
+        name: "HTML 5",
+        icon: html,
     },
     {
-      name: "JavaScript",
-      icon: javascript,
+        name: "CSS 3",
+        icon: css,
     },
     {
-      name: "TypeScript",
-      icon: typescript,
+        name: "JavaScript",
+        icon: javascript,
     },
     {
-      name: "React JS",
-      icon: reactjs,
+        name: "TypeScript",
+        icon: typescript,
     },
     {
-      name: "Redux Toolkit",
-      icon: redux,
+        name: "React JS",
+        icon: reactjs,
     },
     {
-      name: "Tailwind CSS",
-      icon: tailwind,
+        name: "Redux Toolkit",
+        icon: redux,
     },
     {
-      name: "Node JS",
-      icon: nodejs,
+        name: "Tailwind CSS",
+        icon: tailwind,
     },
     {
-      name: "MongoDB",
-      icon: mongodb,
+        name: "Node JS",
+        icon: nodejs,
     },
     {
-      name: "Three JS",
-      icon: threejs,
+        name: "MongoDB",
+        icon: mongodb,
     },
     {
-      name: "git",
-      icon: git,
+        name: "Three JS",
+        icon: threejs,
     },
     {
-      name: "figma",
-      icon: figma,
-    },
-  ];
-  
-  
-  const testimonials = [
-    {
-      testimonial:
-        "I thought it was impossible to make a website as beautiful as our product, but Arpan proved me wrong.",
-      name: "Sara Lee",
-      designation: "CFO",
-      company: "Acme Co",
-      image: "https://randomuser.me/api/portraits/women/4.jpg",
+        name: "Git",
+        icon: git,
     },
     {
-      testimonial:
-        "I've never met a web developer who truly cares about their clients' success like Arpan does.",
-      name: "Chris Brown",
-      designation: "COO",
-      company: "DEF Corp",
-      image: "https://randomuser.me/api/portraits/men/5.jpg",
+        name: "Figma",
+        icon: figma,
+    },
+];
+
+const testimonials = [
+    {
+        testimonial:
+            "I thought it was impossible to make a website as beautiful as our product, but Arpan proved me wrong.",
+        name: "Sara Lee",
+        designation: "CFO",
+        company: "Acme Co",
+        image: "https://randomuser.me/api/portraits/women/4.jpg",
     },
     {
-      testimonial:
-        "After Arpan optimized our website, our traffic increased by 50%. We can't thank him enough!",
-      name: "Lisa Wang",
-      designation: "CTO",
-      company: "456 Enterprises",
-      image: "https://randomuser.me/api/portraits/women/6.jpg",
-    },
-  ];
-  
-  const projects = [
-    {
-      name: "PassOp",
-      description:
-        "Web-based platform that allows users to save edit delete copy passord site and username.It stores many site url username and password in a single place.",
-      tags: [
-        {
-          name: "react",
-          color: "blue-text-gradient",
-        },
-        {
-          name: "mongodb",
-          color: "green-text-gradient",
-        },
-        {
-          name: "tailwind",
-          color: "pink-text-gradient",
-        },
-      ],
-      image: passmanager,
-      source_code_link: "https://github.com/CrazyArpan/Password-Manager",
+        testimonial:
+            "I've never met a web developer who truly cares about their clients' success like Arpan does.",
+        name: "Chris Brown",
+        designation: "COO",
+        company: "DEF Corp",
+        image: "https://randomuser.me/api/portraits/men/5.jpg",
     },
     {
-      name: "Tech Quiz",
-      description:
-        "A simple tech quiz based on programming languages.",
-      tags: [
-        {
-          name: "react",
-          color: "blue-text-gradient",
-        },
-        {
-          name: "tailwind",
-          color: "green-text-gradient",
-        },
-        
-      ],
-      image: tquiz,
-      source_code_link: "https://github.com/CrazyArpan/Tech-Quiz",
+        testimonial:
+            "After Arpan optimized our website, our traffic increased by 50%. We can't thank him enough!",
+        name: "Lisa Wang",
+        designation: "CTO",
+        company: "456 Enterprises",
+        image: "https://randomuser.me/api/portraits/women/6.jpg",
     },
-  ];
-  
-  export { services, technologies, testimonials, projects };
+];
+
+const projects = [
+    {
+        name: "PassOp",
+        description:
+            "Web-based platform that allows users to save, edit, delete, and copy passwords, site URLs, and usernames. It stores many site URLs, usernames, and passwords in a single place.",
+        tags: [
+            {
+                name: "React",
+                color: "blue-text-gradient",
+            },
+            {
+                name: "MongoDB",
+                color: "green-text-gradient",
+            },
+            {
+                name: "Tailwind CSS",
+                color: "pink-text-gradient",
+            },
+        ],
+        image: passmanager,
+        source_code_link: "https://github.com/CrazyArpan/Password-Manager",
+    },
+    {
+        name: "Tech Quiz",
+        description:
+            "A simple tech quiz based on programming languages.",
+        tags: [
+            {
+                name: "React",
+                color: "blue-text-gradient",
+            },
+            {
+                name: "Tailwind CSS",
+                color: "green-text-gradient",
+            },
+        ],
+        image: tquiz,
+        source_code_link: "https://github.com/CrazyArpan/Tech-Quiz",
+    },
+];
+
+export { services, technologies, testimonials, projects };

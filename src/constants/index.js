f734e58d18222207902f0d437d6ec756f8216f9a
CrazyArpan <166271755+CrazyArpan@users.noreply.github.com>
2024-12-02 22:26:59 +0530

    Update index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -156,23 +156,6 @@ const projects = [
     image: passmanager,
     source_code_link: "https://github.com/CrazyArpan/Password-Manager",
   },
-  {
-    name: "Tech-Quiz",
-    description:
-      "A simple tech quiz based on programing languages",
-    tags: [
-      {
-        name: "react",
-        color: "blue-text-gradient",
-      },
-      {
-        name: "tailwind",
-        color: "pink-text-gradient",
-      },
-    ],
-    image: tquiz,
-    source_code_link: "https://github.com/CrazyArpan/Tech-Quiz",
-  },
 ];
 
 // Exporting all data
